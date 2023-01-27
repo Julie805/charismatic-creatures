cb6135904759e31443ef25299a6091f406cdcb1d
Julie M <seejulielee@gmail.com>
2023-01-27 13:08:12 -0800

    Added hamburger-reveal navigation feature

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
+const tagline = document.querySelector('.tagline')
 
-function getOpenStatus() {
-  const openStatus = document.getElementById('open-status')
-  const currentDay = "Sunday"
-  const currentTime = "9:00"
-  if currentDay 
-}
+//Transforms the hamburger into an x and reveals the nav menu
+hamburger.addEventListener("click", function() {
+  hamburger.classList.toggle("active")
+  navMenu.classList.toggle("active") 
+  tagline.classList.toggle("active")
+})
+
+//Hides the nav menu links when user clicks on one of the links 
+document.querySelectorAll(".nav-link").forEach(function(link) {
+  link.addEventListener("click", function() {
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+    document.querySelector('.close-icon').style.display ='none'
+    document.querySelector('.menu-icon').style.display ='block'
+  })
+})
+
+
+
+
+// function getOpenStatus() {
+//   const openStatus = document.getElementById('open-status')
+//   const currentDay = "Sunday"
+//   const currentTime = "9:00"
+//   if currentDay 
+// }
